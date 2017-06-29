1f520cc67f6d4a2a68f34dc09b4de0afdd6bcc3b
panindraprakash <panindraprakash@gmail.com>
2017-06-29 17:07:02 +0530

    Update Sum Of Square Difference.cpp

--- Sum Of Square Difference.cpp
+++ Sum Of Square Difference.cpp
@@ -9,12 +9,12 @@ void main()
 	int i,asum=0,bsum=0;
 	for(i=0;i<=100;i++)
 	{
-		asum=asum+sqrtl(i);
+		asum=asum+sqrt(i);
 		bsum=bsum+i;
 	}
 	printf("\n\nTHE SUM OF THE SQUARE OF THE FIRST 100 NACTURAL NUMBER IS %d\n\n",asum);
 	printf("\n\nTHE SQUARE OF THE SUM OF THE FIRST 100 NACTURAL NUMBER IS %d\n\n",bsum);
-	printf("\n\DIFFERENCE BETWEEN THE SUM OF THE SQUARE OF THE FIRST 100 NACTURAL NUMBER AND THE SQUARE OF THE SUM OF FIRST 100 NACTURAL NUMBER  IS %d\n\n",bsum-asum);
+	printf("\n\DIFFERENCE BETWEEN THE SUM OF THE SQUARE OF THE FIRST 100 NACTURAL NUMBER AND THE SQUARE OF THE SUM OF FIRST 100 NACTURAL NUMBER  IS %d\n\n",sqrt(bsum)-asum);
 
 }
 
